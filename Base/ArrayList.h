a97cb02e0e1ec6adceb8bdb310e1a6781c7a9de1
腾 陈 <enumhack@163.com>
2020-12-02 10:59:26 +0800

    feat: null->nullptr

--- Base/ArrayList.h
+++ Base/ArrayList.h
@@ -9,12 +9,12 @@ class ArrayList {
         m_nCapacity = nCap < 16 ? 16 : nCap;
         m_nSize = 0;
         m_cArray = new T[m_nCapacity];
-        assert(m_cArray != NULL);
+        assert(m_cArray != nullptr);
     }
     ~ArrayList() {
-        if (m_cArray != NULL) {
+        if (m_cArray != nullptr) {
             delete[] m_cArray;
-            m_cArray = NULL;
+            m_cArray = nullptr;
         }
     }
 
@@ -35,21 +35,76 @@ class ArrayList {
     } 
 
     //! 设置index位置的元素
-	T set(int index, T element);
+	T set(int index, T element) {
+        rangecheck(index, false);
+        T oldElement = m_cArray[index]; 
+        m_cArray[index] = element;
+        return 
+    }
     //! 返回index位置对应的元素
-	T get(int index); 
+	T get(int index) {
+        rangecheck(index, false);
+        return m_cArray[index];
+    } 
     //! 往index位置添加元素
-	void add(int index, T element); 
+	void insert(int index, T element) {
+        rangecheck(index, true);
+        prereserve(m_nSize + 1);
+        for (int i = m_nSize; i > index; --i) {
+            m_cArray[i] = m_cArray[i-1];
+        }
+        m_cArray[index] = element;
+        m_nSize++;
+    }
     //! 添加元素到最后面
-	void add(T element); 
+	void append(T element) {
+        insert(m_nSize, element);
+    }
     //! 删除index位置对应的元素
-	T remove(int index); 
+	T remove(int index) {
+        rangecheck(index, false);
+
+        T oldElement = m_cArray[index];  
+        for (int i = index; i < m_nSize; ++i) {
+            m_cArray[i] = m_cArray[i+1];
+        }
+        m_cArray[--m_nSize] = nullptr;
+
+        return oldElement;
+    }
     //! 清除所有元素
-	void clear(); 
+	void clear() {
+        for (int i = 0; i < m_nSize; i++) {
+            m_cArray[i] = nullptr;
+        }
+        m_nSize = 0;
+    } 
     
  private:
-    
-    
+    void rangecheck(int index, bool isAdd) {
+        if (isAdd) {
+            assert(index >= 0 && index <= m_nSize);
+        } else {
+            assert(index >= 0 && index < m_nSize);
+        }
+    }
+
+    void prereserve(int nCap) {
+        if (nCap < m_nCapacity) {
+            return; 
+        }
+
+        m_nCapacity = 2 * m_nCapacity;
+        T* cArray = new T[m_nCapacity];
+        assert(cArrays != nullptr); 
+
+        for (int i = 0; i < m_nSize; ++i) {
+            cArrays[i] = m_cArrayp[i];
+        }
+        
+        delete[] m_cArray;
+        m_cArray = cArray;
+    }
 
  private:
     int m_nSize;
